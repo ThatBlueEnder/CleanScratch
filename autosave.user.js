de8115dc373d81b6ecba9433cea75cbd1710af7c
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-24 21:45:31 -0500

    Add files via upload

---
+++ autosave.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         auto save
+// @match        *://scratch.mit.edu/projects/*/editor/*
+// @grant        GM_addStyle
+// ==/UserScript==
+(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+function check(changes, observer) {
+    if(document.querySelector('div[class^=save-status_save-now]')) {
+        document.querySelector('div[class^=save-status_save-now]').click();
+    }
+}
---
+++ create.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         create checker
+// @match        *://scratch.mit.edu/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+function check(changes, observer) {
+    if(document.querySelector('#login-dropdown')) {
+        observer.disconnect();
+        GM_addStyle('#project-create{display:none!important;}');
+    }
+}
+(new MutationObserver(doublecheck)).observe(document, {childList: true, subtree: true});
+function doublecheck(changes, observer) {
+    if(document.querySelector('.login-item')) {
+        observer.disconnect();
+        GM_addStyle('.create{display:none!important;}');
+    }
+}
+(new MutationObserver(triplecheck)).observe(document, {childList: true, subtree: true});
+function triplecheck(changes, observer) {
+    if(document.querySelector('.account-nav')) {
+        observer.disconnect();
+        GM_addStyle('.create{display: inline-block!important;}');
+    }
+}
