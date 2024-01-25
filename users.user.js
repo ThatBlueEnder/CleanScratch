b39d17915b1a127828c93a67126884c2b2fa8a71
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-24 21:43:09 -0500

    Update users.user.js

--- users.user.js
+++ users.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name         users
-// @match        https://scratch.mit.edu/users/*
+// @name         users checker
+// @match        *://scratch.mit.edu/users/*
 // @grant        GM_addStyle
 // @run-at       document-start
 // ==/UserScript==
-GM_addStyle('#content{display:none;}');
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
 function check(changes, observer) {
     if(document.querySelector('.user-name')) {
         observer.disconnect();
-        if(window.location.pathname.startsWith("/users/"+document.querySelector(".user-name").textContent+"/")){
-            GM_addStyle('#content{display:revert;}');
+        if(!window.location.pathname.startsWith("/users/"+document.querySelector(".user-name").textContent+"/")){
+            GM_addStyle('#content{display:none;}');
         }
+    }else if(document.querySelector('#login-dropdown')) {
+        observer.disconnect();
+        GM_addStyle('#content{display:none;}');
     }
 }
