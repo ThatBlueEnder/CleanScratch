1c7316957bb24aec91a579bbcc7fa174f9c90914
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-29 21:50:47 -0500

    Update users.user.js

--- users.user.js
+++ users.user.js
@@ -3,6 +3,8 @@
 // @match        *://scratch.mit.edu/users/*
 // @grant        GM_addStyle
 // @run-at       document-start
+// @version      1.0
+// @downloadURL  https://github.com/ThatBlueEnder/CleanScratch/raw/main/users.user.js
 // ==/UserScript==
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
 function check(changes, observer) {
