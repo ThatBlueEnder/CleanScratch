1df7dd4a041e773be2344367cd764af1a518ee16
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-29 21:50:32 -0500

    Update projects.user.js

--- projects.user.js
+++ projects.user.js
@@ -3,6 +3,8 @@
 // @match        https://scratch.mit.edu/projects/*
 // @grant        GM_addStyle
 // @run-at       document-start
+// @version      1.0
+// @downloadURL  https://github.com/ThatBlueEnder/CleanScratch/raw/main/projects.user.js
 // ==/UserScript==
 GM_addStyle('#view{display:none;}');
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
