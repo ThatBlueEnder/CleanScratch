b598cc1e75f5007d4cc48065060d749e64e0c1a9
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-29 21:48:41 -0500

    Update autosave.user.js

--- autosave.user.js
+++ autosave.user.js
@@ -2,6 +2,8 @@
 // @name         auto save
 // @match        *://scratch.mit.edu/*
 // @grant        GM_addStyle
+// @version      1.0
+// @downloadURL  https://github.com/ThatBlueEnder/CleanScratch/raw/main/autosave.user.js
 // ==/UserScript==
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
 function check(changes, observer) {
