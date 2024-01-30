06bfb789ed165539e944fe694e0ce7c0cf76b68e
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-29 21:49:46 -0500

    Update create.user.js

--- create.user.js
+++ create.user.js
@@ -3,6 +3,8 @@
 // @match        *://scratch.mit.edu/*
 // @grant        GM_addStyle
 // @run-at       document-start
+// @version      1.0
+// @downloadURL  https://github.com/ThatBlueEnder/CleanScratch/raw/main/create.user.js
 // ==/UserScript==
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
 function check(changes, observer) {
