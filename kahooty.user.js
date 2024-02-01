f68e2423c3a4dc2f2a3fd58cd90e392c9bddb68e
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-01-31 23:20:22 -0500

    Update kahooty.user.js

--- kahooty.user.js
+++ kahooty.user.js
@@ -3,7 +3,9 @@
 // @version      1.0
 // @match        *://kahoot.it/*
 // @grant        GM_addStyle
-// @downloadURL  https://github.com/ThatBlueEnder/CleanScratch/raw/main/kahooty.user.js
 // ==/UserScript==
-GM_addStyle('[class^=theme__Background]{background:none!important}')
-GM_addStyle('[class^=background__Background]{background:none!important}')
+GM_addStyle('[class^=theme__Background] {background: none !important}');
+GM_addStyle('[class^=background__Background] {background: none !important}');
+GM_addStyle('[class^=collaboration-navigation__PageMainContent] {background: none !important}');
+GM_addStyle('[class^=theme__ScrollWrapper]::-webkit-scrollbar {display: none;}');
+GM_addStyle('[class^=theme__ScrollWrapper] {-ms-overflow-style: none; scrollbar-width: none;}')
