18ba03e2190d0fdffc3d6c5c01e986e096500c60
ThatBlueEnder <148366561+ThatBlueEnder@users.noreply.github.com>
2024-02-10 14:28:24 -0500

    Update blue.user.js

--- blue.user.js
+++ blue.user.js
@@ -2,7 +2,7 @@
 // @name         blue
 // @match        *://*/*
 // @grant        GM_addStyle
-// @version      2.0
+// @version      3.0
 // @downloadURL  https://github.com/ThatBlueEnder/CleanScratch/raw/main/blue.user.js
 // ==/UserScript==
 GM_addStyle('*{user-select: none;} html,body{overscroll-behavior: none !important;}');
@@ -12,3 +12,7 @@ document.body.addEventListener('dragstart', event => {
 document.body.addEventListener('drop', event => {
   event.preventDefault();
 });
+var meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "user-scalable=no";
+document.head.appendChild(meta);
